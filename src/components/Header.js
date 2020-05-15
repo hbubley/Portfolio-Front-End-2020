2fa6ecf2d2cc8fa9419ec5e4b3c7b492bd1254f8
HALEY_BUBLEY <HaleyBubley@gmail.com>
2020-05-14 23:16:36 -0500

    Started and added basic layout

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="header-container">
+        <div className="header-content">
+          <h1>Haley Bubley</h1>
+          <h3>&lt;Front-End Developer&gt;</h3>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+export default function Nav() {
+    return (
+        <div>
+        <nav>
+          <a href="blank">Portfolio</a>
+        </nav> 
+        </div>
+    )
+}
