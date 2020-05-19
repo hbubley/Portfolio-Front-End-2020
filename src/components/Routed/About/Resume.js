c8a07095d89b7660688e31d953ce47a1dcc7a228
HALEY_BUBLEY <HaleyBubley@gmail.com>
2020-05-19 12:29:10 -0500

    navbar fix

--- src/components/Routed/About/Resume.js
+++ src/components/Routed/About/Resume.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Resume() {
     return (
         <div className='about-resume'>
-            <a href='https://drive.google.com/file/d/1uNahcvPM2PwRTdLpxvzsRe98qp6yf3IJ/view?usp=sharing' target='_blank' rel="noopener noreferrer"><img src='https://res.cloudinary.com/dum4u7sro/image/upload/v1589686855/icons_-_Desktop_7_gt9lvz.png' alt='' /></a>
+            <a href='https://drive.google.com/open?id=1zzdF0Mfajj_9SNXMuybl1hMobJ-xYiYT' target='_blank' rel="noopener noreferrer"><img src='https://res.cloudinary.com/dum4u7sro/image/upload/v1589686855/icons_-_Desktop_7_gt9lvz.png' alt='' /></a>
         </div>
     )
 }
